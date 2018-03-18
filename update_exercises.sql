4207ee47089ed396bb9b6d6057a415448e0ba324
mdm911 <mdm911@yahoo.com>
2018-03-18 14:38:28 -0500

    Completed update_exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,19 +1,19 @@
 use codeup_test_db;
 
--- select 'These are all the albums' as 'Info';
--- select * from albums;
--- UPDATE albums
--- SET sales = sales * 10;
+select 'These are all the albums' as 'Info';
+select * from albums;
+UPDATE albums
+SET sales = sales * 10;
 
--- select 'These albums were released before 1980' as 'Info';
--- select * from albums where release_date < 1980;
--- UPDATE albums
--- SET release_date = release_date - 100
--- WHERE release_date < 1980;
+select 'These albums were released before 1980' as 'Info';
+select * from albums where release_date < 1980;
+UPDATE albums
+SET release_date = release_date - 100
+WHERE release_date < 1980;
 
 
 select 'These are the albums by Michael Jackson' as 'Info';
-select * from albums where artist = 'Michael Jackson';
+select * from albums where artist = 'Peter Jackson';
 UPDATE albums
 SET artist = 'Peter Jackson'
 WHERE artist = 'Michael Jackson';

5dc0f316e86d345bf8e2fdd0dd5515915214b058
mdm911 <mdm911@yahoo.com>
2018-03-18 02:59:19 -0500

    Corrected release_date not printing for Lonely Hearts Club

--- select_exercises.sql
+++ select_exercises.sql
@@ -3,8 +3,8 @@ use codeup_test_db;
 select 'These are all the albums by Pink Floyd' as 'Info';
 select * from albums where artist = 'Pink Floyd';
 
-select 'This is the year Sgt Pepper''s Lonely Hearts Club Band was released' as 'Info';
-select release_date from albums where name = 'Sgt Pepper''s Lonely Hearts Club Band';
+select 'This is the year Sgt. Pepper''s Lonely Hearts Club Band was released' as 'Info';
+select release_date from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 select 'The album Nevermind is classified as:' as 'Info';
 select genre from albums where name = 'Nevermind';

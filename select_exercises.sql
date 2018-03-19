adfdc30db85f31106459e3d7b66ebe48aced41e4
mdm911 <mdm911@yahoo.com>
2018-03-19 11:16:53 -0500

    Changed select statements on select_Exercises.sql

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,7 +1,7 @@
 use codeup_test_db;
 
 select 'These are all the albums by Pink Floyd' as 'Info';
-select * from albums where artist = 'Pink Floyd';
+select name from albums where artist = 'Pink Floyd';
 
 select 'This is the year Sgt. Pepper''s Lonely Hearts Club Band was released' as 'Info';
 select release_date from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
@@ -10,8 +10,11 @@ select 'The album Nevermind is classified as:' as 'Info';
 select genre from albums where name = 'Nevermind';
 
 select 'These albums were released in the 1990s:' as 'Info';
-select * from albums where release_date BETWEEN 1990 and 1999;
+select * from albums where release_date BETWEEN 1990 AND 1999;
+
+select 'These albums have less than 20 million certified sales' as 'Info';
+select * from albums where sales < 20;
 
 select 'These are the Rock albums:' as 'Info';
-select * from albums where genre = 'Rock';
+select * from albums where genre like '%Rock%';
 

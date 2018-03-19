3febe0e7fbf29ab7216180851323f3a6a029fa57
mdm911 <mdm911@yahoo.com>
2018-03-19 16:08:30 -0500

    Completed where_exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,6 +1,9 @@
 use employees;
 
+select * from employees where first_name in ('Irena', 'Vidya', 'Maya');
 select * from employees where (first_name = 'Irena' or first_name = 'Vidya' OR  first_name = 'Maya') and gender = 'M';
+
+select * from employees where last_name like 'E%';
 
 select * from employees where last_name like '%E' or last_name like 'E%';
 

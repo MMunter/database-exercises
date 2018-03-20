b9e24422330ee39ad6182daa53f11cf8b6eeea64
mdm911 <mdm911@yahoo.com>
2018-03-20 14:09:57 -0500

    Completed group_by_exercises.sql

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,34 +1,15 @@
 use employees;
 
-#2, 3, & 4
-select * from employees where first_name in ('Irena', 'Vidya', 'Maya')
-order by last_name, first_name ASC;
-
-select * from employees where (first_name = 'Irena' or first_name = 'Vidya' OR  first_name = 'Maya') and gender = 'M';
-
-# last name starts with E
-select * from employees where last_name like 'E%'
-order by emp_no DESC ;
-
-# last name starts with E or ends with E
-select * from employees where last_name like '%E' or last_name like 'E%'
-order by emp_no DESC ;
-
 # last name starts with E AND ends with E
 select concat(first_name,  ' ', last_name) from employees where last_name like '%E' and last_name like 'E%'
 # can also use last_name like 'E%E'
 # order by emp_no DESC
 
-
-select * from employees where hire_date BETWEEN '1990-01-01' and '1999-12-31';
-# can also use hire_date like '199%'
-
-select * from employees where birth_date like '%-12-25';
-
-select * from employees where last_name like '%q%';
-
-select concat(first_name, ' ', last_name, ' has worked here for ', datediff(now(), hire_date), ' days.') from employees where birth_date like '%-12-25' and hire_date BETWEEN '1990-01-01' and '1999-12-31'
+select concat(first_name, ' ', last_name, ' has worked here for ', datediff(now(), hire_date), ' days.')
+from employees
+where birth_date
+      like '%-12-25' and hire_date BETWEEN '1990-01-01' and '1999-12-31'
 order by birth_date ASC, hire_date desc;
 
-select * from employees where last_name like '%q%' and last_name not like '%qu%';
+
 
---
+++ group_by_exercises.sql
@@ -0,0 +1,16 @@
+use employees;
+
+select distinct title from titles;
+
+select first_name, last_name
+from employees where last_name like 'E%E'
+GROUP BY first_name, last_name;
+
+select last_name, count(*) from employees
+where last_name like '%q%' AND
+  last_name not like '%qu%'
+GROUP BY last_name
+order by count(*);
+
+select count(*), gender from employees where first_name in ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
